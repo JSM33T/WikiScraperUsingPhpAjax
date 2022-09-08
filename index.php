e62a05e106780cad9b585118ca5115799ae1738d
Jasmeet Singh <43501139+JSM33T@users.noreply.github.com>
2022-09-08 15:57:29 +0530

    Update index.php

--- index.php
+++ index.php
@@ -74,14 +74,12 @@
                      <option value="19">19</option><option value="20">20</option>      
                      <option value="21">21</option><option value="22">22</option><option value="23">23</option><option value="24">24</option>
                      <option value="25">25</option><option value="26">26</option><option value="27">27</option><option value="28">28</option>
-                     <option value="29">29</option> <option value="30">30</option><option value="31">31</option>
-                                             
+                     <option value="29">29</option> <option value="30">30</option><option value="31">31</option>                        
                   </select>
                </div>
                <div class="col-md-6">                                
                   <button name="submit" class=" btn btn-secondary" onClick="trigger();">FIND</button>
                </div>
-         
             <div class="col-sm-12 mb-40">
                <div class="widget">
                
